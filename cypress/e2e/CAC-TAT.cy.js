864f54d6b754e0ce7d8d2b5c29aa78335e73fc19
NatanaelBalbino <natanael.m.m.balbino@gmail.com>
2025-06-30 22:33:46 -0300

    exercicios_4_5_e_6_completos

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -69,4 +69,77 @@ describe('Central de Atendimento ao Cliente TAT', () => {
     .get('.error')
     .should('be.visible')
   })
+  it('campo telefone continua vazio quando preenchido com valor não numérico', () => {
+
+    cy
+    .get('#phone')
+    .type('abcdefghij')
+    .should('have.value', '')
+  })
+  it('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', () => {
+    cy
+    .get('#phone-checkbox')
+    .click()
+
+    cy
+    .get('#firstName')
+    .type('Natanael')
+
+    cy
+    .get('#lastName')
+    .type('Balbino')
+
+    cy
+    .get('#email')
+    .type('natanael@gmail.com')
+
+    cy
+    .get('#open-text-area')
+    .type('Mensagem de teste')
+
+    cy
+    .get('button[type="submit"]')
+    .click()
+
+    cy
+    .get('.error')
+    .should('be.visible')
+  })
+  it('preenche e limpa os campos nome, sobrenome, email e telefone', () => {
+    cy
+    .get('#firstName')
+    .type('Natanael')
+    .should('have.value', 'Natanael')
+    .clear()
+    .should('have.value', '')
+
+    cy
+    .get('#lastName')
+    .type('Balbino')
+    .should('have.value', 'Balbino')
+    .clear()
+    .should('have.value', '')
+
+    cy
+    .get('#email')
+    .type('natanael.balbino@gmail.com')
+    .should('have.value', 'natanael.balbino@gmail.com')
+    .clear()
+    .should('have.value', '')
+
+    cy
+    .get('#phone')
+    .type('11993999293')
+    .should('have.value', '11993999293')
+    .clear()
+    .should('have.value', '')
+  })
+  it('exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios', () => {
+    cy
+    .get('button[type="submit"]')
+    .click()
+
+    cy.get('.error')
+    .should('be.visible')
+  })
 })
